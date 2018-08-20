38abdf7f5c9d068140565b7150ef25873f95bc00
zengxiaoyu <1769322419@qq.com>
2018-08-20 16:30:33 +0800

    修改话题update

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -10,12 +10,18 @@ use SebastianBergmann\CodeCoverage\Report\Xml\Totals;
 
 class TopicsController extends Controller
 {
-    //
+    //发布话题
     public function store(TopicRequest $request,Topic $topic){
         $topic->fill($request->all());
         $topic->user_id = $this->user()->id;
         $topic->save();
         return $this->response->item($topic, new TopicTransformer())
             ->setStatusCode(201);
+    }
+    //修改话题
+    public function update(TopicRequest $request, Topic $topic){
+        $this->authorize('update',$topic);
+        $topic->update($request->all());
+        return $this->response->item($topic,new TopicTransformer());
     }
 }
