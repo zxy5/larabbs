18404b5303c6c6ed9c802df10518541556f829cf
zengxiaoyu <1769322419@qq.com>
2018-08-22 11:22:42 +0800

    删除回复

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -9,10 +9,9 @@ use App\Models\Reply;
 
 class RepliesController extends Controller
 {
-    //
+    //回复话题
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
-//        return 's';
         $reply->content = $request->contents;
         $reply->topic_id = $topic->id;
         $reply->user_id = $this->user()->id;
@@ -20,5 +19,14 @@ class RepliesController extends Controller
 
         return $this->response->item($reply, new ReplyTransformer())
             ->setStatusCode(201);
+    }
+    //删除回复
+    public function destroy(Topic $topic,Reply $reply){
+        if($reply->topic_id != $topic->id){
+            return $this->response->errorBadRequest();
+        }
+        $this->authorize('destroy',$reply);
+        $reply->delete();
+        return $this->response->noContent();
     }
 }
