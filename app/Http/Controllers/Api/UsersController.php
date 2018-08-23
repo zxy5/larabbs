5112015c5e0f183c8c95390f4d5cc64f53ed2116
zengxiaoyu <1769322419@qq.com>
2018-08-23 17:10:41 +0800

    活跃用户接口

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -56,5 +56,10 @@ class UsersController extends Controller
         return $this->response->item($user,new UserTransformer());
     }
 
+    public function activedIndex(User $user)
+    {
+        return $this->response->collection($user->getActiveUsers(),new UserTransformer());
+    }
+
 
 }
